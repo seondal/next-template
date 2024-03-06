2beb07a32452e0bfddfd377a9242f27b3ccb9edd
seondal <sseondal@gmail.com>
2024-03-06 20:21:42 +0900

    ✨ feat :  Google Adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { GA_TAG } from "@/constants/env";
+import { ADSENSE_ACCOUNT, GA_TAG } from "@/constants/env";
 
 export const metadata: Metadata = {
   title: "Title",
   description: "description",
+  other: {
+    "google-adsense-account": ADSENSE_ACCOUNT,
+  },
 };
 
 export default function RootLayout({
@@ -18,12 +21,11 @@ export default function RootLayout({
     <html lang="kor">
       <body>
         <header></header>
-        <main className="flex flex-col justify-center items-center">
-          {children}
-        </main>
+        <main className="text-center max-w-96 m-auto">{children}</main>
         <GoogleAnalytics gaId={GA_TAG} />
         <Analytics />
-        <footer className="flex flex-col justify-center items-center">
+        <footer className="text-center p-4">
+          <div>문의, 기능제안, 오류신고 편하게 해주세요 !</div>
           <a href="https://toss.me/seondal">이 사이트 만든 사람</a>
         </footer>
       </body>
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,1 +1,2 @@
 export const GA_TAG = process.env.NEXT_PUBLIC_GA_TAG || "";
+export const ADSENSE_ACCOUNT = process.env.NEXT_PUBLIC_ADSENSE_ACCOUNT || "";
